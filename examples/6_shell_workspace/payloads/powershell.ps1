4dbb4bbeb1c75d6b762916d52515bd6ea898409c
Lthreec <514676651@qq.com>
2026-05-28 04:38:58 +0800

    fix: ui

--- examples/6_shell_workspace/payloads/powershell.ps1
+++ examples/6_shell_workspace/payloads/powershell.ps1
@@ -3,3 +3,4 @@ Write-Host "pwd=$PWD"
 Write-Host "PYRUNS_EXAMPLE_ENV=$env:PYRUNS_EXAMPLE_ENV"
 
 python -c "import os; print('python_env_marker=' + os.environ.get('PYRUNS_EXAMPLE_ENV', ''))"
+python -c "import os; print('helloword')"
